fc97085a78743d170fcdae4c15449673d017a402
Hirisci <www.alan92@icloud.com>
2022-06-24 16:30:49 +0200

    add bonus

--- js/main.js
+++ js/main.js
@@ -2,18 +2,49 @@ var app = new Vue({
   el: '#app',
   data: {
     albums: [],
+    genreList: [],
+    filter: 'all',
   },
   created() {
     axios
-      .get('http://localhost/Esercizi/php-ajax-dischi/server/listalbum.php')
-      .then(function (response) {
+      .get('http://localhost/Esercizi/php-ajax-dischi/server/listalbum.php', {
+        params: {
+          genre: 'all',
+        },
+      })
+      .then((response) => {
         // handle success
         this.albums = response.data;
-        console.log(this.albums);
+        this.generateGenreList();
       })
-      .catch(function (error) {
+      .catch((error) => {
         // handle error
         console.log(error);
       });
+  },
+  methods: {
+    generateGenreList() {
+      this.albums.forEach((elm) => {
+        if (!this.genreList.includes(elm.genre)) {
+          this.genreList.push(elm.genre);
+        }
+      });
+    },
+    receiveList(filter) {
+      axios
+        .get('http://localhost/Esercizi/php-ajax-dischi/server/listalbum.php', {
+          params: {
+            genre: filter,
+          },
+        })
+        .then((response) => {
+          // handle success
+          this.albums = response.data;
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    },
   },
 });
--- server/listalbum.php
+++ server/listalbum.php
@@ -1,6 +1,16 @@
 <?php
 
 require_once "database.php";
-
 header('Content-Type: application/json');
-echo json_encode($database);
+
+if ($_GET["genre"] == "all") {
+    echo json_encode($database);
+} else {
+    $arr = [];
+    foreach ($database as $album) {
+        if ($album["genre"] == $_GET["genre"]) {
+            $arr[] = $album;
+        }
+    }
+    echo json_encode($arr);
+}
